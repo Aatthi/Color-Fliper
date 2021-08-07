485497ba88e715b914e849cb441772263b952760
Aatthitthae <aathitthan11@gmail.com>
2021-08-08 08:02:01 +1000

    update

--- Calculator/app.js
+++ Calculator/app.js
@@ -3,6 +3,9 @@ window.addEventListener('DOMContentLoaded',init);
     const opts = ["*", "/", "+", "-", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", "."]
     const spec = ["*", "/", "+", "-"]; 
 
+    function addOutput(e) {
+
+    }
 
 function init(){
     document.title = "Javascript Calculator"
@@ -28,8 +31,31 @@ function init(){
      const main = document.createElement('div');
      main.classList.add('main');
      main.style.width = "100%";
-     main.style.border = "1px solid red"
+     main.style.border = "1px solid red";
+
      container.appendChild(main)
 
+     opts.forEach(function(val){
+         console.log(val)
+         buttonMaker(val, addOutput)
+     })
 
+     function buttonMaker(txt, myFunction){
+         let btn = document.createElement('button');
+         btn.setAttribute('type', 'button')
+         btn.style.width = '25%';
+         btn.style.lineHeight = '50px';
+         btn.style.margin = '1%';
+         btn.style.background = 'rgba(32, 114, 221, 0.815)';
+         btn.style.border = "none"
+         btn.style.borderRadius = '10px'
+
+
+         btn.val = txt;
+         btn.textContent = txt;
+         btn.addEventListener('click', myFunction);
+         main.appendChild(btn)
+         
+
+     }
 }

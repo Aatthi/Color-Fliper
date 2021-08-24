7d3f055a8c2e473695f13122a417a4d5c32abcbc
Aatthitthae <aathitthan11@gmail.com>
2021-08-24 19:47:38 +1000

    Update end.js

--- Quiz/end.js
+++ Quiz/end.js
@@ -2,16 +2,42 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore')
 const mostRecentScore = localStorage.getItem('mostRecentScore')
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGHSCORES = 5;
+console.log(highScores)
+
 finalScore.innerText = mostRecentScore;
 
 
+
+
 username.addEventListener('keyup', () => {
-    console.log(username.value);
-    saveScoreBtn.disabled = !username.value;
+        saveScoreBtn.disabled = !username.value;
+})
+
+saveScoreBtn.addEventListener('click', function() {
+    username.value = '';
 })
 
 
 saveHighScore = e => {
     console.log('clicked');
     e.preventDefault();
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    }
+
+    highScores.push(score); 
+
+    highScores.sort((a,b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("index.html")
+
+    console.log(highScores);
 };
